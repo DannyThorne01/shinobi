f3c1d14a719b5a89ee435bf7752873b4bf8be51b
dsb289 <dsb289@cornell.edu>
2026-03-01 00:43:27 -0400

    Elementary TCP Server

--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
 #include "stdio.h"  // printf, fprintf
+#include <stdlib.h>      // exit()
+#include "string.h"      // memset()
+#include <errno.h>
 #include <arpa/inet.h>   // inet_ntop() - converts binary IP to string
 #include <netinet/in.h>  // sockaddr_in structures
+#include <netdb.h>       // getaddrinfo(), addrinfo
+
+#include <sys/wait.h>    // wait()
+#include <sys/signal.h>
 #include <sys/socket.h>  // socket types/constants  
 #include <sys/types.h>   // data types (size_t)
-#include <netdb.h>       // getaddrinfo(), addrinfo
-#include "string.h"      // memset()
 
+#include <unistd.h>      // fork()
+#include <signal.h>
 
 /*
   struct addrinfo {
@@ -26,46 +33,130 @@
       unsigned char       sin_zero[8];    // Same size as struct sockaddr
   };
 */
+#define MY_PORT "3490"
+#define QUEUE_SIZE 20
+
+void addr_to_str(struct sockaddr *addr, char *res){
+  void* res_addr;
+  char *ipver;
+  if(addr->sa_family == AF_INET){
+      ipver = "IPv4";
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)addr; 
+      res_addr = &ipv4->sin_addr;
+      uint8_t *bytes = (uint8_t *)res_addr;
+      printf("IPv4 RAW BYTES: %02x %02x %02x %02x\n", 
+        bytes[0], bytes[1], bytes[2], bytes[3]);
+  }else{
+      ipver = "IPv6";
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) addr;
+      res_addr = &ipv6->sin6_addr;
+      uint8_t *bytes = (uint8_t *)res_addr;
+      printf("IPv6 RAW BYTES: %02x %02x %02x %02x %02x %02x %02x %02x"
+             " %02x %02x %02x %02x %02x %02x %02x %02x\n",
+        bytes[0],  bytes[1],  bytes[2],  bytes[3],
+        bytes[4],  bytes[5],  bytes[6],  bytes[7],
+        bytes[8],  bytes[9],  bytes[10], bytes[11],
+        bytes[12], bytes[13], bytes[14], bytes[15]);
+    }
+  inet_ntop(addr->sa_family, res_addr, res, INET6_ADDRSTRLEN);
+  printf("%s: %s\n", ipver, res);
+}
+
+void sigchld_handler(int s)
+{
+  while(wait(NULL) > 0);
+}
 
 int main(){
   int status;
-  char* site = "laminar-interview.vercel.app";
   struct addrinfo hints = {}; //search criteria
   struct addrinfo *servinfo, *winner; // Results lists + best match
-  char ipstr[INET6_ADDRSTRLEN];// Buffer for IP string (46 chars max )
-  // configure DNS Lookup (hints)
+  char ipstr[INET6_ADDRSTRLEN];
+  socklen_t addr_size;
+  struct sigaction sa;
+  int yes = 1;
+  int new_fd;
+  struct sockaddr_storage their_addr;
+ 
   memset(&hints, 0, sizeof(hints)); // Fill form with zeros
   // fill specifc boxes
   hints.ai_family = 0; // "Try everything both IPv4 and IPv6"
-  hints.ai_socktype = 1; // TCP please
+  hints.ai_socktype = SOCK_STREAM; // TCP please
   hints.ai_flags = 0; // I'm a server
 
-  if((status == getaddrinfo(site, "80", &hints, &servinfo)) != 0){
+  if((status = getaddrinfo(NULL, MY_PORT, &hints, &servinfo)) != 0){
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
     return 1;
   }
+  if(servinfo==NULL){
+    printf("getaddrinfo: could not find result");
+    return 1;
+  }
 
-  for (winner = servinfo; winner !=NULL; winner = winner->ai_next){
-    void *addr;
-    char *ipver;
-    if(winner->ai_family == AF_INET){
-      ipver = "IPv4";
-      struct sockaddr_in *ipv4 = (struct sockaddr_in *) winner->ai_addr; 
-      addr = &ipv4->sin_addr;
-      uint8_t* bytes = (uint8_t*)addr;  // Cast to bytes
-      printf("IPv4 RAW BYTES: %02x %02x %02x %02x\n", 
-         bytes[0], bytes[1], bytes[2], bytes[3]);
-    }else{
-      ipver = "IPv6";
-      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) winner->ai_addr;
-      addr = &ipv6->sin6_addr;
-      uint8_t* bytes = (uint8_t*)addr;  // Cast to bytes
-      printf("IPv4 RAW BYTES: %02x %02x %02x %02x\n", 
-         bytes[0], bytes[1], bytes[2], bytes[3]);
-    }
-    inet_ntop(winner->ai_family, addr, ipstr, sizeof ipstr);
-    printf("%s: %s\n", ipver, ipstr);
+  winner = servinfo;
+  // make a socket and bind 
+  addr_to_str(servinfo->ai_addr, ipstr);
+  //int socket(int domain, int type, int protocol);
+  int sockfd = socket(winner->ai_family, winner->ai_socktype, winner->ai_protocol);
+
+  // so we can reuse the IP
+  if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
+    printf("setsockopt : error");
+    return 1;
+  }
+  // int bind(int sockfd, struct sockaddr *my_addr, int addrlen);
+  if (bind(sockfd, winner->ai_addr, winner->ai_addrlen) == -1){
+    printf("bind: error");
+    return 1;
   }
   freeaddrinfo(servinfo);
-  return 0;
+
+  if(listen(sockfd, QUEUE_SIZE) == -1){
+    printf("listen: error");
+    return 1;
+  }
+  printf("listening on %s:%s\n", ipstr, MY_PORT);
+  fflush(NULL);
+
+  sa.sa_handler= sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+  while(1){
+    addr_size=  sizeof(struct sockaddr_storage);
+
+    //int accept(int sockfd, void *addr, int *addrlen);
+    if((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &addr_size))==-1){
+      perror("accept");
+      continue;
+    }
+    char peer_name[INET6_ADDRSTRLEN];
+    addr_to_str((struct sockaddr *)&their_addr, peer_name);
+    printf("server: got connection from %s\n", peer_name);
+    fflush(NULL);
+
+    if (!fork()){ // this is the child process
+      close(sockfd); // child doesn;t need a listener 
+      const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "Hello World!\n";
+      if (send(new_fd, response, strlen(response), 0) == -1){
+        perror("send");
+      }
+      close(new_fd);
+      exit(0);
+    }
+    close(new_fd); //parent doesn't need this anymore
+  }
+
+return 0;
+
 }
